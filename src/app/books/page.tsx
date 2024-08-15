fe9f6478f0c653c9fe4567d68d33ab8b3ef1c6bf
lj520github <JimmyYLJ@outlook.com>
2024-08-15 13:18:11 +0800

    initial commit

---
+++ src/app/books/page.tsx
@@ -0,0 +1,12 @@
+export default async function Page() {
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between fs-12">
+                <h1>books</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+         
+            </div>
+        </div>
+    );
+}
